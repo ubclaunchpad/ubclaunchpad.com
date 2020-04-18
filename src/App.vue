065b1cab29a25f3ac5a7ce7b1d376f14eb24db17
Robert Lin <robert@bobheadxi.dev>
2020-04-17 18:34:42 -0700

    all: init typescript

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
-export default {
+export default Vue.extend({
   name: 'App',
   components: {
     HelloWorld
   }
-}
+});
 </script>
 
 <style>
