0a7332ef4cea9b4dc365c915783d7b21897d5803
Robert Lin <robert@bobheadxi.dev>
2020-04-18 15:20:05 -0700

    add about section

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-
+  <div id="container" class="container is-widescreen">
+    <div class="columns is-vcentered">
+      <div class="column">
+        <img src="@/assets/about.png">
+      </div>
+      <div class="pad-32 column is-three-fifths">
+        <h2>Who we are</h2>
+        <p>
+          We’re a student-run software engineering team devoted to building software projects in a
+          collaborative and professional environment.
+        </p>
+        <p>
+          Our primary objective is to be the best space at UBC for students to meet other tech
+          enthusiasts, build projects in a team setting, gain experience with professional development
+          tools and principles, and share their knowledge with others.
+        </p>
+        <p>
+          We form teams based on common interests and project ideas of our own choosing, and hack on
+          our projects to completion! We are also lucky enough to have sponsors which help provide
+          our teams with necessary resources, like servers, to help us achieve our goals.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
