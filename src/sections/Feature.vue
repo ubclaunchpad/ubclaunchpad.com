931b625f1ce313b4952f3b316a48dcd42ee0acb2
Robert Lin <robertlin1@gmail.com>
2020-04-22 08:27:51 -0700

    add responsive layouts for landing sections (#53)

--- src/sections/Feature.vue
+++ src/sections/Feature.vue
@@ -1,24 +1,33 @@
 <template>
-  <div id="container" class="container is-widescreen">
-    <div class="columns is-vcentered">
-      <div class="pad-32 column is-three-fifths">
-        <div class="columns is-vcentered animated slideInDown">
-          <div class="column is-narrow">
-            <h1>
-              UBC Launch Pad
-            </h1>
+  <div id="feature-container" class="container is-widescreen">
+    <div class="columns is-vcentered is-tablet">
+      <div class="pad-32 column is-three-fifths-tablet">
+        <div class="animated slideInDown">
+          <!-- column-based layout with the "aside" description visible on desktop (hide-mobile, hide-tablet) -->
+          <div class="columns is-vcentered is-hidden-mobile is-hidden-tablet-only">
+            <div class="column is-narrow">
+              <h1>UBC Launch Pad</h1>
+            </div>
+            <div class="column title-divider is-narrow" />
+            <div class="column">
+              <p class="main title-aside">Software Engineering<br>Design Team</p>
+            </div>
           </div>
-          <div id="aside-divider" class="column is-narrow is-hidden-mobile" />
-          <div class="column">
-            <p id="aside" class="main">
-              Software Engineering<br>Design Team
-            </p>
+
+          <!-- simple title, description layout visible tablet -->
+          <div class="is-hidden-desktop">
+            <h1>UBC Launch Pad</h1>
+            <p class="main is-hidden-desktop margin-bottom-16 margin-top-16"><b>Software Engineering Design Team</b></p>
           </div>
         </div>
 
+        <!-- put image between title and text on small screens -->
+        <img src="@/assets/landing.png" class="small-image is-hidden-tablet" >
+
         <div class="animated fadeIn">
           <p class="secondary">
-            A leading student-run software club devoted to building applications in a collaborative and professional environment.
+            A leading student-run software club based in the University of British Columbia devoted
+            to building applications in a collaborative and professional environment.
           </p>
           <p v-if="applicationsOpen" class="secondary">
             <i>{{ season }} applications are now open!</i>
@@ -38,7 +47,8 @@
       </div>
 
       <div class="column">
-        <img src="@/assets/landing.png">
+        <!-- put image on right side on desktop -->
+        <img src="@/assets/landing.png" class="is-hidden-mobile" >
       </div>
     </div>
   </div>
@@ -68,12 +78,27 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
-#aside {
+#feature-container {
+  @media(max-width: $tablet) {
+    margin-top: 80px;
+  }
+}
+
+.small-image {
+  width: 100%;
+  max-width: 300px;
+  margin-top: 16px;
+  margin-bottom: 32px;
+}
+
+.title-aside {
   font-size: 16px;
   line-height: 19px;
+  margin-top: 0px;
   margin-bottom: 0px;
 }
-#aside-divider {
+
+.title-divider {
   border-left: 2px solid $white;
   height: 42px;
   margin-left: 8px;
