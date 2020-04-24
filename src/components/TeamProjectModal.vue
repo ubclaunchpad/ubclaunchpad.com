29a7d7d6e6b7f422d1d8eaccd70346fa5efa7d1a
Robert Lin <robert@bobheadxi.dev>
2020-04-23 21:44:27 -0700

    simply modal background style

--- src/components/TeamProjectModal.vue
+++ src/components/TeamProjectModal.vue
@@ -72,13 +72,9 @@ export default Vue.extend({
 
   .modal-bg-container {
     position: absolute;
-    width: 100%;
-    height: 100%;
-
     .modal-bg {
       overflow: hidden;
       mix-blend-mode: overlay;
-      width: 100%;
     }
   }
 
